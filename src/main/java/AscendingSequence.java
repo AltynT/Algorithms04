531a16b964b37308da96a25090ae50e61a0985ae
Your Name <altyntoro@gmail.com>
2022-05-09 07:59:50 -0500

    Algorithms04 project's first commit

--- src/main/java/AscendingSequence.java
+++ src/main/java/AscendingSequence.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class AscendingSequence {
+public class AscendingSequence {
+
+    public int[] ascendingSequenceAlgorithm(int start, int end, int step) {
+        if (step > 0 && start < end) {
+            int[] result = new int[(end - start + 1) / step];
+
+            for(int i = 0; i < result.length; i ++) {
+                if (start <= end) {
+                    result[i] = start;
+                    start += step;
+                }
+            }
+
+            return result;
+        }
+        return new int[]{};
+    }
 }
+
+
